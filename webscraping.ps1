f1f8de5d3b48e25f91f88a39cc53cee9428326d7
Prateek Singh <Prateeksingh1590@gmail.com>
2016-11-29 23:49:51 +0530

    Update webscraping.ps1

--- webscraping.ps1
+++ webscraping.ps1
@@ -11,18 +11,34 @@ $j1 = foreach($item in $j1)
 }
 
 $a = (iwr -uri "http://staging.laughfactory.com/jokes/technology-jokes")
-$j2 = $a.ParsedHtml.getElementsByTagName("p")|?{$_.id -like "joke_*"}| %{$_.innertext}
-$j2 = foreach($item in ($a.ParsedHtml.getElementsByTagName("p")| %{$_.innertext}))
+$j2 = foreach($item in ($a.ParsedHtml.getElementsByTagName("p")|?{$_.id -like "joke_*"}| %{$_.innertext}))
 {
     ''|select @{n='Category';e={'joke'}}, @{n='text';e={[System.Web.HttpUtility]::HtmlDecode($item)}}
 }
 
 
 $b = (iwr -Uri "http://staging.laughfactory.com/jokes/technology-jokes/2")
-$j3 = $a.ParsedHtml.getElementsByTagName("p")|?{$_.id -like "joke_*"}| %{$_.innertext}
-$j3 = foreach($item in ($a.ParsedHtml.getElementsByTagName("p")| %{$_.innertext}))
+$j3 = foreach($item in ($b.ParsedHtml.getElementsByTagName("p")|?{$_.id -like "joke_*"}| %{$_.innertext}))
 {
     ''|select @{n='Category';e={'joke'}}, @{n='text';e={[System.Web.HttpUtility]::HtmlDecode($item)}}
 }
 
-$joke = $j1+$j2+$j3
+$j4 = foreach($item in (((iwr -uri "http://tambal.azurewebsites.net/").content |ConvertFrom-Json).joke))
+{
+    ''|select @{n='Category';e={'joke'}}, @{n='text';e={[System.Web.HttpUtility]::HtmlDecode($item)}}
+}
+
+$c = (iwr -Uri "http://staging.laughfactory.com/jokes/technology-jokes/3")
+$j5 = foreach($item in ($c.ParsedHtml.getElementsByTagName("p")|?{$_.id -like "joke_*"}| %{$_.innertext}))
+{
+    ''|select @{n='Category';e={'joke'}}, @{n='text';e={[System.Web.HttpUtility]::HtmlDecode($item)}}
+}
+
+$d= (iwr -Uri "http://www.techrepublic.com/article/the-geekiest-tech-jokes-on-the-internet/")
+($d.ParsedHtml.getElementsByTagName("div") |?{$_.classname -eq 'content'}).innerhtml
+$j5 = foreach($item in ($d.ParsedHtml.getElementsByTagName("p")| %{$_.innertext|?{$_ -like "*. *."}}))
+{
+    ''|select @{n='Category';e={'joke'}}, @{n='text';e={[System.Web.HttpUtility]::HtmlDecode($item)}}
+}
+
+$joke = $j1+$j2+$j3+$j4
